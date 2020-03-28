1eaac5da27a6fade3958421473d48af58f8c0436
Ubuntu <ubuntu@ip-172-31-85-99.ec2.internal>
2020-03-28 18:50:47 +0000

    desenv

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-1dvp-12345"
-    key    = "state/hackaton-cicd-deploy"
+    bucket = "hackathon-fiap-1dvp-334131"
+    key    = "state/hackathon-cicd-deploy"
     region = "us-east-1"
   }
 }
